82c99c2bfa369b9455d81e179526586073532e0f
assm201359-sys <assm.201359@lsb.edu.co>
2026-08-14 12:44:23 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const horario=document.getElementById('horario');
+const btn=document.getElementById('btn-toggle');
+horario.style.display='none';
+btn.addEventListener('click',()=>{
+horario.style.display=horario.style.display==='none'?'block':'none';
+});
+const botonColor=document.getElementById('btn-color');
+const miTabla=document.querySelector('.tabla');
+botonColor.addEventListener('click',()=>{miTabla.classList.toggle('modo-hacker');
+});
